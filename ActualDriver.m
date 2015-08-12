5c8182e18e08cebd422f897de5d54296f63a45b8
s4weng <steve_weng@hotmail.ca>
2015-08-12 01:52:21 -0400

    Added robot duties

--- ActualDriver.m
+++ ActualDriver.m
@@ -22,6 +22,86 @@ while true
     h1 = Sigmoid(final * Theta1');
     h1 = [1 h1];
     h2 = Sigmoid(h1 * Theta2');
-    [p, p] = max(h2)
+    [p, p] = max(h2);
     
+    
+% Up Brake Left Right Output
+%  0   0    0     0      1
+%  0   0    0     1      2
+%  0   0    1     0      3
+%  0   0    1     1      4
+%  0   1    0     0      5
+%  0   1    0     1      6
+%  0   1    1     0      7
+%  0   1    1     1      8
+%  1   0    0     0      9
+%  1   0    0     1      10
+%  1   0    1     0      11
+%  1   0    1     1      12
+%  1   1    0     0      13
+%  1   1    0     1      14
+%  1   1    1     0      15
+%  1   1    1     1      16
+
+% all keys are typed out because they are held down unless released
+    if p == 1
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 2
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyPress(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 3
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyPress(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 5
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyPress(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 6
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyPress(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyPress(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 7
+        driver.keyRelease(java.awt.event.KeyEvent.VK_UP);
+        driver.keyPress(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyPress(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 9
+        driver.keyPress(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 10
+        driver.keyPress(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyPress(java.awt.event.KeyEvent.VK_RIGHT);
+        
+    elseif p == 9
+        driver.keyPress(java.awt.event.KeyEvent.VK_UP);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_SPACE);
+        driver.keyLeft(java.awt.event.KeyEvent.VK_LEFT);
+        driver.keyRelease(java.awt.event.KeyEvent.VK_RIGHT);
+    
+    else
+        break;
+        
+    end
+    
+    fprintf('The program has stopped.');
 end
