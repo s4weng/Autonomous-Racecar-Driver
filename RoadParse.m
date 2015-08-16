ee5a15d31e8e38a51c342c3fe3539bdfd8267d96
s4weng <steve_weng@hotmail.ca>
2015-08-16 14:28:44 -0400

    newly trained params, run 4

--- RoadParse.m
+++ RoadParse.m
@@ -24,23 +24,23 @@ for i = 1:rows
     third = y(i, 3);
     fourth = y(i, 4);
     
-    if ((first == 0) && (second == 0) && (third == 0) && (fourth == 0))
+    if ((first == 1) && (second == 1) && (third == 1) && (fourth == 1))
         output = 1;
-    elseif ((first == 0) && (second == 0) && (third == 0) && (fourth == 1))
+    elseif ((first == 1) && (second == 1) && (third == 1) && (fourth == 2))
         output = 2;
-    elseif ((first == 0) && (second == 0) && (third == 1) && (fourth == 0))
+    elseif ((first == 1) && (second == 1) && (third == 2) && (fourth == 1))
         output = 3;
-    elseif ((first == 0) && (second == 1) && (third == 0) && (fourth == 0))
+    elseif ((first == 1) && (second == 2) && (third == 1) && (fourth == 1))
         output = 4;
-    elseif ((first == 0) && (second == 1) && (third == 0) && (fourth == 1))
+    elseif ((first == 1) && (second == 2) && (third == 1) && (fourth == 2))
         output = 5;
-    elseif ((first == 0) && (second == 1) && (third == 1) && (fourth == 0))
+    elseif ((first == 1) && (second == 2) && (third == 2) && (fourth == 1))
         output = 6;
-    elseif ((first == 1) && (second == 0) && (third == 0) && (fourth == 0))
+    elseif ((first == 2) && (second == 1) && (third == 1) && (fourth == 1))
         output = 7;
-    elseif ((first == 1) && (second == 0) && (third == 0) && (fourth == 1))
+    elseif ((first == 2) && (second == 1) && (third == 1) && (fourth == 2))
         output = 8;
-    elseif ((first == 1) && (second == 0) && (third == 1) && (fourth == 0))
+    elseif ((first == 2) && (second == 1) && (third == 2) && (fourth == 1))
         output = 9;
     else
         output = -1;
